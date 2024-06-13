d1ba38845bb775405cdeab3e3f8c97945266b1b1
KrlosDev <carlosb@bbhint.com>
2024-06-12 22:20:34 -0500

    second version of custom kicks, integrating paypal and changing the overall design making it more generic for future use

---
+++ backend/data/products.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    name: 'Air Jordan 1 Low OG "Black/Gorge Green"',
+    image: '/images/image-1718245929665.png',
+    description:
+      'Step into greatness with the Air Jordan 1 Low OG. Updated color and texture gives the all-time favorite fresh identity while staying true to the original design.',
+    brand: 'Nike Jordans',
+    category: 'Shoes',
+    price: 140,
+    countInStock: 12,
+    rating: 4.5,
+    numReviews: 12,
+  },
+ 
+];
+
+export default products;
