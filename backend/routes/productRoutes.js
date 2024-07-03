4357bfe6b05684dd9e358ca9d009f427792f07db
KrlosDev <carlosb@bbhint.com>
2024-07-03 15:34:57 -0500

    reverting changes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,16 +9,16 @@ import {
   createProductReview,
   getTopProducts,
 } from '../controllers/productController.js';
-import { /*protect,*/ admin } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 import checkObjectId from '../middleware/checkObjectId.js';
 
-router.route('/').get(getProducts).post(/*protect,*/ admin, createProduct);
-router.route('/:id/reviews').post(/*protect,*/ checkObjectId, createProductReview);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 router.get('/top', getTopProducts);
 router
   .route('/:id')
   .get(checkObjectId, getProductById)
-  .put(/*protect,*/ admin, checkObjectId, updateProduct)
-  .delete(/*protect,*/ admin, checkObjectId, deleteProduct);
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
