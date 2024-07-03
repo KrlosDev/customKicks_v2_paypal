59de027f54690c283169d3aa7ec5be2e1be9e724
KrlosDev <carlosb@bbhint.com>
2024-07-03 15:55:02 -0500

    did not work

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,13 +12,13 @@ import {
 import { protect, admin } from '../middleware/authMiddleware.js';
 import checkObjectId from '../middleware/checkObjectId.js';
 
-router.route('/').get(getProducts).post(/*protect, admin,*/createProduct);
-router.route('/:id/reviews').post(/*protect,*/ checkObjectId, createProductReview);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 router.get('/top', getTopProducts);
 router
   .route('/:id')
   .get(checkObjectId, getProductById)
-  .put(/*protect, admin,*/checkObjectId, updateProduct)
-  .delete(/*protect, admin,*/checkObjectId, deleteProduct);
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,17 +14,17 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(/*protect, admin,*/getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 router
   .route('/profile')
-  .get(/*protect,*/ getUserProfile)
-  .put(/*protect,*/ updateUserProfile);
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router
   .route('/:id')
-  .delete(/*protect, admin,*/deleteUser)
-  .get(/*protect, admin,*/getUserById)
-  .put(/*protect, admin,*/updateUser);
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
